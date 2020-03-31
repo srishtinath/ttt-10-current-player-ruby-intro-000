cd277f9efd17d0adb5c75c93dbbf1d34fac57ccc
Srishti Nath <srishti.nath@gmail.com>
2020-03-31 16:25:12 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board).even?
-    puts "X"
-  else
+  if turn_count(board) % 2 == 0
     puts "O"
+  else
+    puts "X"
   end
 end
